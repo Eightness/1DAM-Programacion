6dd6c1f03756253ca99f2783529d0a8816603067
Albert <albertlb08@gmail.com>
2023-03-20 10:13:27 +0100

    Avances

--- 6.POO/Casos-practicos/Agenda-Contactos/Book.java
+++ 6.POO/Casos-practicos/Agenda-Contactos/Book.java
@@ -1,49 +1,115 @@
 //Albert Lozano Blasco - Agenda y contactos
 //1 DAM - Programación
 
+import java.util.Scanner;
+
 //Book class
 public class Book {
     
     //Class Attributes
-    
-
-    //----------------------------------------------------------
-
-    //Constructors
-
-    //Empty constructor
-
-
-    //Full constructor
-
+    private static final int maxContacts = 100;
+    private static int numContacts = 0;
+    private static Contact[] contacts = new Contact[maxContacts];
+    private static Scanner input = new Scanner(System.in);
 
     //----------------------------------------------------------
 
     //Functions
 
-    //Function to show contacts
+    //Function to get the input (contact name)
+    public static String inputName() {
+        String inputName;
+        do {
+            System.out.print("Introduce nombre: ");
+            inputName = input.nextLine();
+        } while (nameExists(inputName)); 
+        return inputName;
+    }
 
+    //Function to get the input (contact phone)
+    public static String inputPhone() {
+        System.out.print("Introduce teléfono: ");
+        String inputPhone = input.nextLine();
+        return inputPhone;
+    }
+
+    //Function to get the input (contact mail)
+    public static String inputMail() {
+        System.out.print("Introduce correo: ");
+        String inputMail = input.nextLine();
+        return inputMail;
+    }
+
+    //Function to get the input (search)
+    public static String inputSearch() {
+        System.out.print("Buscar: ");
+        String inputSearch = input.nextLine();
+        return inputSearch;
+    }
+
+    //Function to show contacts
+    public void showContacts() {
+        if (isEmpty()) {
+            System.out.println();
+            System.out.println("No hay contactos.");
+        } else {
+            System.out.println();
+            for (int i = 0; i < numContacts; i++) {
+                System.out.println(i + ". " + contacts[i].showContact());
+            }
+        }
+    }
 
     //Function to add a contact
-
+    public void addContact() {
+        if (isFull()) {
+            System.out.println();
+            System.out.println("La agenda está llena.");
+        } else {
+            System.out.println();
+            contacts[numContacts] = new Contact(inputName(), inputPhone(), inputMail());
+            numContacts++;
+            System.out.println();
+            System.out.println("Contacto añadido con éxito.");
+        }
+    }
 
     //Function to remove a contact
+    public void removeContact() {
 
+    }
 
-    //Function to search for a contact
+    //Function to search for a contact (by name, phone, mail or overall)
+    public void searchContact(int option) {
 
+    }
 
     //Function to see if the book is empty
-
+    public boolean isEmpty() {
+        if (contacts[0] == null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
     //Function to see if the book is full
+    public boolean isFull() {
+        if (contacts[maxContacts - 1] != null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
     
-    
-    //Function to see if a name exists
-
-    //----------------------------------------------------------
-
-    //Setters and Getters
-
+    //Function to see if a name exists in the book
+    public static boolean nameExists(String name) {
+        for (int i = 0; i < numContacts; i++) {
+            if (contacts[i].sameName(name)) {
+                return true;
+            }
+        }
+        return false;
+    }
     
 }
--- 6.POO/Casos-practicos/Agenda-Contactos/Contact.java
+++ 6.POO/Casos-practicos/Agenda-Contactos/Contact.java
@@ -25,30 +25,86 @@ public class Contact {
         this.mail = mail;
     }
 
-
     //----------------------------------------------------------
 
     //Functions
 
     //Function to show contact information
-
+    public String showContact() {
+        String contact = getName() + "\t" + getPhone() + "\t" + getMail();
+        return contact;
+    }
 
     //Function to see if a determined String is found
-
+    public boolean isFound(String search) {
+        if (getName().contains(search) || getPhone().contains(search) || getMail().contains(search)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
     //Function to see if a determined String is found in its name
+    public boolean nameFound(String search) {
+        if (getName().contains(search)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
+    //Function to see if a determined String is found in its phone
+    public boolean phoneFound(String search) {
+        if (getPhone().contains(search)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-    //Function to see if a determined String is found its phone
+    //Function to see if a determined String is found in its mail
+    public boolean mailFound(String search) {
+        if (getMail().contains(search)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-
-    //Function to see if a determined String is found its mail
-
-
+    //Function to see if a name already exists in the contact
+    public boolean sameName(String name) {
+        if (getName().equalsIgnoreCase(name)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
     //----------------------------------------------------------
 
     //Setters and Getters
+    public void setName(String name) {
+        this.name = name;
+    }
 
-    
+    public String getName() {
+        return name;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
 }
