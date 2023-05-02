e0d878396368092109a6a554a2c0e826d15e9213
Albert <albertlb08@gmail.com>
2023-05-02 14:17:07 +0200

    Created attributes for class Almacen + Const + SG

--- 6.POO/TienDAM/Almacen.java
+++ 6.POO/TienDAM/Almacen.java
@@ -3,7 +3,7 @@ public class Almacen {
     //Atributos
     private int maxArticulos;
     private Articulo[] articulos;
-    private int numArticulos;
+    private int numArticulos = 0;
 
     //Constructores
     //Constructor vacío
@@ -13,7 +13,30 @@ public class Almacen {
 
     //Constructor completo
     public Almacen(int maxArticulos, Articulo[] articulos) {
-        
+
     }
+
+    //Getters y Setters
+    //Setters
+    public void setArticulos(Articulo[] articulos) {
+        this.articulos = articulos;
+    }
+
+    public void setMaxArticulos(int maxArticulos) {
+        this.maxArticulos = maxArticulos;
+    }
+
+    //Getters
+    public Articulo[] getArticulos() {
+        return articulos;
+    }
+    
+    public int getMaxArticulos() {
+        return maxArticulos;
+    }
+
+    //Métodos
+    //Método para ver/imprimir un Almacén
+    
     
 }
