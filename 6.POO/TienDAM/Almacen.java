daff86ed076b963b0643dc239319a3aba25dab2a
Albert <albertlb08@gmail.com>
2023-05-02 14:13:40 +0200

    Updated class Articulo

--- 6.POO/TienDAM/Almacen.java
+++ 6.POO/TienDAM/Almacen.java
@@ -1,3 +1,19 @@
 public class Almacen {
+
+    //Atributos
+    private int maxArticulos;
+    private Articulo[] articulos;
+    private int numArticulos;
+
+    //Constructores
+    //Constructor vacío
+    public Almacen() {
+
+    }
+
+    //Constructor completo
+    public Almacen(int maxArticulos, Articulo[] articulos) {
+        
+    }
     
 }
