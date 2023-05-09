dbfde22401497b50e2f438a2dd12288c86196675
Albert <albertlb08@gmail.com>
2023-05-09 14:40:20 +0200

    exercise 8 done

--- 7. Excepciones/Ejercicio8.java
+++ 7. Excepciones/Ejercicio8.java
@@ -3,19 +3,38 @@ import java.util.InputMismatchException;;
 
 public class Ejercicio8 {
 
-    //Atributes
+    //Attributes
     static Scanner input = new Scanner(System.in);
 
     //Methods
-    static int getValidIntegerInput(String message) {
-        int input = 0;
-        System.out.print("Introduce un número entero: ");
-        return input;
-    }
 
+    //This method serves as a template for getting user input
+    static int getValidIntegerInput(String message) {
+        //Declaring and initializing variables
+        int res = 0;
+        boolean check = false;
+        //do-while structure to ask the user until its validated
+        do {
+            System.out.println();
+            System.out.print(message);
+            try {
+                //Executes these lines and tries to catch an exception
+                res = input.nextInt();
+                check = true;            
+            } catch (InputMismatchException e) {
+                //If exception catched, show this message: 
+                System.out.println("Tipo de dato introducido incorrecto.");
+                input.nextLine();
+            }
+        } while (!check);
+        //Return user input
+        return res;
+    }
     
     public static void main(String[] args) {
-
-        
+        //Calling method
+        int a = getValidIntegerInput("Introduce un número entero: ");
+        //Show result
+        System.out.println("El valor introducido por el usuario es: " + a);
     }
 }
