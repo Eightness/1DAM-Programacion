6ae3edd8ac2e625d05a38bf6781e0707f26ccef2
Albert <albertlb08@gmail.com>
2023-05-09 14:28:39 +0200

    added atributes and methods

--- 7. Excepciones/Ejercicio8.java
+++ 7. Excepciones/Ejercicio8.java
@@ -2,9 +2,20 @@ import java.util.Scanner;
 import java.util.InputMismatchException;;
 
 public class Ejercicio8 {
+
+    //Atributes
     static Scanner input = new Scanner(System.in);
+
+    //Methods
+    static int getValidIntegerInput(String message) {
+        int input = 0;
+        System.out.print("Introduce un número entero: ");
+        return input;
+    }
+
     
     public static void main(String[] args) {
+
         
     }
 }
